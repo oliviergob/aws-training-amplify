c31935f3ea6a0a4b07a34c9039018dd0acd767e1
Olivier Gobilliard <olivier.gob@gmail.com>
2022-02-22 23:21:05 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # aws-training-amplify
+
+Simple Web App to be deployed in AWS Amplify
+
+## Running the app
+
+`npm start`
+
+## Building the app
+
+`npm run build`
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            }
+          }
+        ]
+    }],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/mystyles.css'
+    }),
+    new HtmlWebpackPlugin({
+      template: 'public/index.html'
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 3000,
+  },
+};
