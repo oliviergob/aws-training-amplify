324cf85240c061fc43af12fdad0dcefd42336915
Olivier Gobilliard <olivier.gob@gmail.com>
2022-04-28 22:59:54 +0100

    Calling API from Front End

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 require('./mystyles.scss');
+
+const site_desciption = document.getElementById('site_description')
+
+
+
+fetch('https://lx09ng1b5i.execute-api.us-east-1.amazonaws.com/prod/configuration').then(response =>{
+    return response.json();
+}).then(data =>{
+    console.log(data.welcome_text);
+    site_desciption.textContent =  data.welcome_text
+})
